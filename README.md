a33f27fdb87a219b8a22125e9d886520a964e7d4
1slander <hectorgomendez@gmail.com>
2022-11-15 08:15:16 +0000

    Add UI

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ What I've learned:
 
 - A better understanding of how functions work.
 - Working with the different types of errors.
+
+This time we will need to work on a UI and also on manipulate the DOM through Javascript.
+
+        - Use the new git branch.
+        - Add a UI and make the game playable by clicking buttons
+        - Put into practice the DOM manipulation methods learnt on the previous unit.
+
+Working on the UI - TOP Things To Do :
+
+In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+1. For now, remove the logic that plays exactly five rounds.
+
+2. Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+
+3. Add a div for displaying results and change all of your console.logs into DOM methods.
+
+4. Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+5. You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
--- script.js
+++ script.js
@@ -1,91 +1,121 @@
 //THE ODIN PROJECT JAVASCRIPT FOUNDATIONS
 //PROJECT 1
 "use strict";
+
+//Variables
 let playerScore = 0;
 let computerScore = 0;
 
+let playerPoints = document.getElementById("playerScorePoints");
+let computerPoints = document.getElementById("computerScorePoints");
+
+let rockBtn = document.querySelector("#rockBtn");
+let paperBtn = document.querySelector("#paperBtn");
+let scissorsBtn = document.querySelector("#scissorsBtn");
+
+// Event Listener
+
+rockBtn.addEventListener("click", function () {
+  let playerSelection = "rock";
+
+  playRound(playerSelection, getComputerChoice());
+  console.log(playerSelection, getComputerChoice());
+});
+paperBtn.addEventListener("click", function () {
+  let playerSelection = "paper";
+
+  playRound(playerSelection, getComputerChoice());
+  console.log(playerSelection, getComputerChoice());
+});
+scissorsBtn.addEventListener("click", function () {
+  let playerSelection = "scissors";
+
+  playRound(playerSelection, getComputerChoice());
+  console.log(playerSelection, getComputerChoice());
+});
 // computer randomly selects one of three option Math.floor(Math.random()* 3)
 
-function getComputerChoice (){
+function getComputerChoice() {
   const computerNumber = Math.round(Math.random() * 2);
-  
-  switch (computerNumber) {
-   case 0:
-    return "rock";
-    break;
-   case 1:
-    return "paper";
-    break;
-   case 2: 
-   return "scissors";
-   break;
-  }
-  
-}
 
+  switch (computerNumber) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+      break;
+  }
+}
 
 // ask for player selection (case-insensitive)
 
-function getPlayerChoice (){
-const playerChoice = prompt("Choose your weapon: Rock, Paper or Scissors?").toLocaleLowerCase();
-return playerChoice;
-}
-
-
+// function getPlayerChoice() {
+//   const playerChoice = prompt(
+//     "Choose your weapon: Rock, Paper or Scissors?"
+//   ).toLocaleLowerCase();
+//   return playerChoice;
+// }
 
 // play a round comparing computer vs player selection and return who is the winner.
 
-function playRound (playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log("Tie");
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    console.log("You Win! Rock beats Scissor");
 
-
-if (playerSelection === computerSelection){
-console.log("Tie");
-} else if (playerSelection == "rock" && computerSelection == "scissors"){
-console.log("You Win! Rock beats Scissor");
-playerScore++;
-
-}else if (playerSelection == "paper" && computerSelection == "rock"){
-console.log("You Win! Paper beats Rock");
-playerScore++;
-} else if (playerSelection == "scissors" && computerSelection == "paper"){
-console.log("You Win! Scissors beats Paper");
-playerScore++;
-} else {
-console.log(`You Lose! ${computerSelection} beats ${playerSelection} `);
-computerScore++;
+    playerScorePts();
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log("You Win! Paper beats Rock");
+    playerScorePts();
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    console.log("You Win! Scissors beats Paper");
+    playerScorePts();
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection} `);
+    computerScorePts();
+  }
 }
+// Scoreboard Update Function
+function playerScorePts() {
+  playerScore += 1;
+  playerPoints.innerText = playerScore;
 }
 
-
+function computerScorePts() {
+  computerScore += 1;
+  computerPoints.innerText = computerScore;
+}
 // play 5 rounds where the score are kept and reports the winner or loser at the end.
 
-function game (){
+// function game (){
 
-let endGameMessage = "";
-//loop for 5 games, track round and wins
-for(let i=0; i<5; i++){
-  const playerSelection = getPlayerChoice();
-  const computerSelection = getComputerChoice ();
-  
-//Play Round
-  playRound(playerSelection, computerSelection);
-  console.log(`Player win totals: ${playerScore}`);
-  console.log(`Computer win totals: ${computerScore}`);
-  
-}
-// determine who won the five round set
-if(playerScore > computerScore){
-  endGameMessage = " YOU WIN!";
-} else if (playerScore === computerScore){
-  endGameMessage = "TIE!";
-} else {
-  endGameMessage = "COMPUTER WINS!";
-}
+// let endGameMessage = "";
+// //loop for 5 games, track round and wins
+// for(let i=0; i<5; i++){
+//   const playerSelection = getPlayerChoice();
+//   const computerSelection = getComputerChoice ();
 
-console.log(`End of the game: ${endGameMessage}`);
+// //Play Round
+//   playRound(playerSelection, computerSelection);
+//   console.log(`Player win totals: ${playerScore}`);
+//   console.log(`Computer win totals: ${computerScore}`);
 
-  
+// }
+// // determine who won the five round set
+// if(playerScore > computerScore){
+//   endGameMessage = " YOU WIN!";
+// } else if (playerScore === computerScore){
+//   endGameMessage = "TIE!";
+// } else {
+//   endGameMessage = "COMPUTER WINS!";
+// }
 
+// console.log(`End of the game: ${endGameMessage}`);
 
-}
-game();
+// }
+// game();
